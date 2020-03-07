6556da19d16d3c01ce2a373e29bf4104a0689c0c
rohit <rohit@rohit-Vostro-1015>
2020-03-07 23:55:01 +0530

    Pushing on repository.

---
+++ student-client-management/src/main/java/com/infy/studentclientmgmt/servcie/StudentClientServiceImpl.java
@@ -0,0 +1,28 @@
+package com.infy.studentclientmgmt.servcie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.infy.studentclientmgmt.model.Student;
+
+@Service
+public class StudentClientServiceImpl implements StudentClientService{
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Override
+	public Student getStudentById(Long id) {
+
+		return restTemplate.getForObject("http://localhost:9090/getStudent/"+id, Student.class);
+	}
+
+	@Override
+	public Student saveStudent(Student stud) {
+
+		return restTemplate.postForObject("http://localhost:9090/addStudent", stud, Student.class);
+	}
+
+	
+}
