aa1d841a244668b036a94b235b501d23650eadb6
rohit <rohit@rohit-Vostro-1015>
2020-03-21 14:31:15 +0530

    Student hystrix service.(not implemented)

---
+++ student-hystrix-dashboard/src/main/java/com/infy/hystrix/StudentHystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.infy.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentHystrixDashboardApplication.class, args);
+	}
+
+}
---
+++ student-hystrix-dashboard/src/test/java/com/infy/hystrix/StudentHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.hystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
